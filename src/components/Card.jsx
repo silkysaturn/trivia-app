902e222886f786e71b619e0bd3d49cdd857d11f5
Maya Oum <maya.oum02@gmail.com>
2025-04-26 00:04:37 -0400

    Create Card.jsx

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import { React} from "react";
+
+export default function Card(){
+    return (
+
+        <div>
+            <p>text</p>
+             <img src="/Sunflower.png"/>
+
+        </div>
+    )
+}
